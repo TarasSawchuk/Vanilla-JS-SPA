d98b263ec901d8318b3bd82d9c90c50ab0097e59
TarasSawchuk <taras338LM@gmail.com>
2019-07-15 16:42:43 +0300

    fifth commit code refactoring

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ let userArr = httpGet('https://jsonplaceholder.typicode.com/users'),
 
 function httpGet(theUrl) {
     let xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", theUrl, false); // false for synchronous request
-        xmlHttp.send(null);
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
     return xmlHttp.responseText;
 }
+
 // Parsing JSON to Objects
 const parseObj = obj => {return JSON.parse(obj);}
 
@@ -16,36 +17,35 @@ listArr = parseObj(listArr);
 
 // Function for printing Users in DOM
 function printUsers(userArr) {
-    let userParentElm = document.getElementsByClassName("user-list")[0],
-        domUser = document.createElement("div");
-
-    userParentElm.appendChild(domUser);
+    let userParentElm = document.getElementsByClassName("user-list")[0];
 
     userArr.forEach( function (user, index) {
-        domUser.innerHTML += `<div class = "users" id = ${index+1} >` + user.id + " " + user.username + `</div>`;
+        userParentElm.innerHTML += `<div class = "users" id = ${index+1} >` + user.id + " " + user.username + `</div>`;
     });
 }
+
 //Onclick function for printing and removing to-do list in DOM
 function printTodo() {
     let clickedUserId = this.getAttribute("id"),
         clickedUsersTodo = listArr.filter( todo => todo.userId == clickedUserId),
         titleContainer = clickedUsersTodo.map( a => a.title ),
         completedContainer = clickedUsersTodo.map( a => a.completed ),
-        todosContainer = document.getElementsByClassName("todos-list")[0],
-        liContainer = document.createElement("ul");
+        todosContainer = document.getElementsByClassName("todos-list")[0];
 
-    todosContainer.appendChild(liContainer);
+    todosContainer.innerHTML = '';
 
-    if(todosContainer.childNodes.length > 2) {
-        todosContainer.removeChild(document.getElementsByTagName("ul")[0]);
-    }
     for(let i = 0; i <= titleContainer.length; i++) {
         if(completedContainer[i] === true){
-            liContainer.innerHTML += `<div><label><input type="checkbox" checked>${titleContainer[i]}</label></div></Br>`;
-        } else {
-            liContainer.innerHTML += `<div><label><input type="checkbox">${titleContainer[i]}</label></div></Br>`;
+            todosContainer.innerHTML += `<div class="todosElm"><label><input type="checkbox" checked><span>${titleContainer[i]}</span></label><button onclick="removeButton(this)">Remove</button></div>`;
+        }else if(completedContainer[i] === false){
+            todosContainer.innerHTML += `<div class="todosElm"><label><input type="checkbox"><span>${titleContainer[i]}</span></label><button onclick="removeButton(this)">Remove</button></div>`;
         }
     }
+}
+
+//Function which delete todos element
+function removeButton(e) {
+    e.parentNode.remove();
 }
 
 //Adding event listener for users class
@@ -55,5 +55,6 @@ function addEventListn() {
         user.addEventListener('click', printTodo);
     });
 }
+
 printUsers(userArr);
 addEventListn();
